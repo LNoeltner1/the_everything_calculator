f80960acd24dac724edd317ca04273dc936f733d
Lauren Noeltner <laurennoeltner@gmail.com>
2020-12-28 20:52:06 -0500

    removed even more unneeded base code

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={"https://fillmurray.com/300/300"} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://www.fillmurray.com/200/300" className="App-logo" alt="logo" />
+  
       </header>
     </div>
   );
