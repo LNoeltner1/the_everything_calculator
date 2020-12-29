0e07701d2fc7f247f880ed910d78d0ce542d2127
Lauren Noeltner <laurennoeltner@gmail.com>
2020-12-28 20:47:09 -0500

    got rid of more unneeded react base code causing errors

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://fillmurray.com/300/300"} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(booksController);
+// app.use(booksController);
 
 app.use(express.static("client/build"));
 
