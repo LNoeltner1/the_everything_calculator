14dda58f70d9ec682cb215fb5bf8f1ec6a118739
Lauren Noeltner <laurennoeltner@gmail.com>
2021-01-01 03:24:15 -0500

    cleaned up home page code, will develop it further later down the line

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
 import TypeMenu from '../components/TypeMenu';
 import { Link } from "react-router-dom";
+import "../App.css";
 
 class Home extends Component {
     render() {
         return (
-            <div className="homeContainer">
+            <div className="row homeContainer">
                 <TypeMenu />
-                <div className="row homePage">
-                        <div className="col-4">
-                            Hello
-                        </div>
-                        <div className="col-8">Lauren</div>
-                    
+                
+                <div className="col-4">
+                    Hello, welcome to my home page. You can convert anything on 
+                    this website, just click on the type of conversion you want 
+                    to calculate on the sidebar.
                 </div>
+                
             </div>
         );
     }
