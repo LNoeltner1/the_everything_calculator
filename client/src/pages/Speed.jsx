b8a85b9154613a5243d08acc159f227767710cea
Lauren Noeltner <laurennoeltner@gmail.com>
2021-02-24 17:36:08 -0500

    had an epiphany and managed to make my code WAY more DRY than before; finished Speed page

--- client/src/pages/Speed.jsx
+++ client/src/pages/Speed.jsx
@@ -1,10 +1,37 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import SideBar from '../components/SideBar';
 import "../App.css";
 
 const Speed = () => {
     const [inputValue, setInputValue] = useState(Number);
+    const [inputUnit, setInputUnit] = useState("");
     const [result, setResult] = useState(Number);
+    const [resultUnit, setResultUnit] = useState(String);
+    const [conversions, setConversions] = useState({});
+    
+    
+    //CONVERSIONS ASSUMING KM/H IS EQUAL TO 1 -- GIVES RATIOS
+    const conversionTable = {
+        "kmph": 1,
+        "mps": 0.277778,
+        "mph" : 0.621371,
+        "knots" : 0.539957,
+        "fps" : 0.911344,
+        "mach" : 0.000809848,
+        "light" : 0.000000000926567,
+        "mpm" : 0.0103562,
+        "kmps" : 0.000277778
+    }
+
+
+    useEffect(() => {
+        loadConversions();
+    }, []);
+
+    function loadConversions() {
+        setConversions(conversionTable);
+        console.log(conversionTable);
+    }
 
     const handleInputChange = (e) => {
         e.preventDefault();
@@ -12,14 +39,58 @@ const Speed = () => {
         setInputValue(value);
     }
 
-    //CONVERSIONS
+    const handleSetResultUnit = (e) => {
+        setResultUnit(e.target.value);
+    }
+    const handleSetInputUnit = (e) => {
+        setInputUnit(e.target.value);
+    }
 
-    //IF/ELSE/IF STATEMENTS TO CALL CONVERSION FNS
+    // KMPH TO ANYTHING
+    const kmphToAnything = () => {
+        setResult(inputValue * conversions[resultUnit]);
+    }
+
+    // ANYTHING TO KMPH
+    const anythingToKmph = () => {
+        setResult(inputValue * (1 / conversions[inputUnit]));
+    }
+
+    // IF NO KMPH, CONVERT WITH THIS EQUATION
+    const convertIt = () => {
+        setResult(inputValue * (1 / conversions[inputUnit]) * conversions[resultUnit])
+    }
+    
+
+    //IF/ELSE/IF STATEMENTS TO CALL CONVERSION FN'S
     const handleConvert = (e) => {
         e.preventDefault();
+        console.log(resultUnit, " resultUnit");
+        console.log(inputUnit, " inputUnit")
         let inputUnitMenu = document.getElementById("unitInput");
         let outputUnitMenu = document.getElementById("unitOutput");
-        console.log(inputUnitMenu.options[inputUnitMenu.selectedIndex].value);
+
+        //checking for unselected units
+        if (inputUnitMenu.options[inputUnitMenu.selectedIndex].value === "0" || outputUnitMenu.options[outputUnitMenu.selectedIndex].value === "0") {
+            alert("You must select a unit.");
+        }
+        // IF INPUT AND OUTPUT UNIT ARE THE SAME
+        else if (inputUnitMenu.options[inputUnitMenu.selectedIndex].value === outputUnitMenu.options[outputUnitMenu.selectedIndex].value) {
+            console.log("no can do, buckaroo");
+            alert("You must select two different units to convert");
+        }
+        else if (inputUnitMenu.options[inputUnitMenu.selectedIndex].value !== "0" && outputUnitMenu.options[outputUnitMenu.selectedIndex].value === "EU") {
+            anythingToKmph();
+            
+        } else if (inputUnitMenu.options[inputUnitMenu.selectedIndex].value === "EU" && outputUnitMenu.options[outputUnitMenu.selectedIndex].value !== "0") {
+            kmphToAnything();
+        } else if (inputUnitMenu.options[inputUnitMenu.selectedIndex].value !== "EU" && outputUnitMenu.options[outputUnitMenu.selectedIndex].value !== "EU") {
+            convertIt();
+        }
+        // IF NOTHING MATCHES
+        else {
+            console.log("Lauren, you done goofed");
+        }
     }
 
     return (
@@ -39,7 +110,7 @@ const Speed = () => {
                     
                         <input id="userInput"  type="text" name="number" value={inputValue} onChange={handleInputChange}/> 
                         <div className="select inputMenu text-center">
-                            <select id="unitInput" className="unitMenu" name="unitInput">
+                            <select id="unitInput" className="unitMenu" name="unitInput" onChange={handleSetInputUnit}>
                                 <option value="0">Select unit:</option>
                                 <option value="kmph">km/hr</option>
                                 <option value="mps">m/s</option>
@@ -62,7 +133,7 @@ const Speed = () => {
                     {/* </div> */}
                     <div className="col-md-5 col-xs-10 col-sm-10 resultBox">Result: {result} 
                         <div className="select outputMenu text-center">
-                            <select id="unitOutput" className="unitMenu" name="unitOutput">
+                            <select id="unitOutput" className="unitMenu" name="unitOutput" onChange={handleSetResultUnit}>
                             <option value="0">Select unit:</option>
                                 <option value="kmph">km/hr</option>
                                 <option value="mps">m/s</option>
